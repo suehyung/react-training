82604a68eda4aa13a949b7049c65cb860c65406c
suehyung <suehyung.shin@gmail.com>
2018-07-24 21:26:47 -0700

    Separate App into its own component

--- app/index.js
+++ app/index.js
@@ -1,18 +1,9 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-require('./index.css');
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello React Lesson!
-      </div>
-    )
-  }
-}
+var React = require('react')
+var ReactDOM = require('react-dom')
+require('./index.css')
+var App = require('./components/App')
 
 ReactDOM.render(
   <App />,
   document.getElementById('app')
-);
+)
