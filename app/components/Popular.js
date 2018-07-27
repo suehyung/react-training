ff2e13b4b3e457442973f39c10acc1de59d85bda
suehyung <suehyung.shin@gmail.com>
2018-07-27 13:09:32 -0700

    Add explanatory comments to Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,15 +1,52 @@
 var React = require('react')
+var PropTypes = require('prop-types')
+
+// stateless functional component for menu bar 
+function SelectLanguage (props) {
+  var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+  return (
+    // use 'className' for css classes
+    <ul className = 'languages'>
+      {languages.map(lang => {
+        return (
+          // iterates thru menu list, green if item matches current state
+          // onSelect arg1 is null bc binding for this.updateLanguage is already
+          // set in constructor. arg2 passes list item to updateLanguage as lang
+          // React requires unique key for each list item
+          <li 
+            style = {lang === props.selectedLanguage ? {color: 'green'}: null}
+            onClick = {props.onSelect.bind(null, lang)}
+            key = {lang}>
+            {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+// performs type checking of props passed to components
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+}
 
 class Popular extends React.Component {
   constructor (props) {
+    // always put first, otherwise this.props will be undefined
     super(props)
+    // sets initial state; don't call .setState() in constructor
     this.state = {
       selectedLanguage: 'All'
     }
-
+    // is in the constructor to ensure that the 'this' inside updateLanguage 
+    // refers to the component instance itself to use setState method
     this.updateLanguage = this.updateLanguage.bind(this)
   }
+
+  // updates state
   updateLanguage(lang) {
+    // React method that changes component state in response to event handler
     this.setState(() => {
       return {
         selectedLanguage: lang
@@ -18,21 +55,13 @@ class Popular extends React.Component {
   }
 
   render() {
-    var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
-    
     return (
-      <ul className = 'languages'>
-        {languages.map(lang => {
-          return (
-            <li 
-              style = {lang === this.state.selectedLanguage ? {color: 'green'}: null}
-              onClick = {this.updateLanguage.bind(null, lang)}
-              key = {lang}>
-              {lang}
-            </li>
-          )
-        })}
-      </ul>
+      <div>
+        <SelectLanguage 
+          selectedLanguage = {this.state.selectedLanguage}
+          onSelect = {this.updateLanguage}
+        />
+      </div>
     )
   }
 }
