b27ec2ea8fb6a99f1d1c0afaad7f5798501096c1
suehyung <suehyung.shin@gmail.com>
2018-07-30 17:41:02 -0700

    Add React Router dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,19 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
+    // sets base path so that pages load/route correctly on browser refresh
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
+  },
+  devServer: {
+    historyApiFallback: true
+    // helps React Router load pages correctly on browser refresh
   },
   plugins: [
     new HtmlWebpackPlugin({
