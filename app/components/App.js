5091c3a0cc7a9f122ea42f8d46ead4544852d242
suehyung <suehyung.shin@gmail.com>
2018-07-30 17:43:40 -0700

    Add React Router and app components

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,35 @@
 var React = require('react')
+var Nav = require('./Nav')
+var Home = require('./Home')
+var Battle = require('./Battle')
 var Popular = require('./Popular')
+// React Router dependencies below
+var ReactRouter = require('react-router-dom')
+var Router = ReactRouter.BrowserRouter
+var Route = ReactRouter.Route
+var Switch = ReactRouter.Switch
 
 class App extends React.Component {
   render() {
     return (
-      <div className = 'container'>
-        <Popular />
-      </div>
+      // Insert components inside Router component, specify component path if needed
+      <Router>
+        <div className = 'container'>
+          <Nav />
+          <Switch> 
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function(){
+              return <p>Page Not Found</p>
+            }} />
+          </Switch>
+      
+        </div>
+      </Router>
     )
-  } 
+  }
+  // 'exact' ensures Home does not load every time 
 }
 
 module.exports = App
