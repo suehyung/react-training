b3e6d76887318cb4d5ad26bf56776f7154ef7503
suehyung <suehyung.shin@gmail.com>
2018-07-30 20:14:51 -0700

    Add functional Battle component input form

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,11 +1,110 @@
 var React = require('react')
+var PropTypes = require('prop-types')
 
-class Battle extends React.Component {
+class PlayerInput extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  // on typing in form, grabs text and sets it as this.state.username
+  handleChange (event) {
+    var value = event.target.value
+    this.setState(function () {
+      return {
+        username: value
+      }
+    })
+  }
+
+  // when user submits, calls onSubmit and passes it id and username to update state and re-render
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
+  }
+
   render () {
     return (
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input
+          id='username'
+          placeholder='GitHub Username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}>
+            Submit
+        </button>
+      </form>
+    )
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
+
+class Battle extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit (id, username) {
+    this.setState(function () {
+      var newState = {}
+      newState[id + 'Name'] = username
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
+      // 'id' is either playerOne or Two, sets new state based on form submit
+      return newState
+    })
+  }
+
+  render () {
+    var playerOneName = this.state.playerOneName
+    var playerTwoName = this.state.playerTwoName
+
+    return (
       <div>
-        BATTLE!
+        <div className='row'>
+          {!playerOneName &&
+            <PlayerInput
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit}
+            />}
+          {!playerTwoName &&
+            <PlayerInput
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit}
+            />}
+        </div>
       </div>
+      // if no playername, then render the form in PlayerInput component, pass id to respective player, and update state on submit
     )
   }
 }
